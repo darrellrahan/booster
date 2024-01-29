989619723ee1a4854e427a2f6f3a77cd93aa0742
Darrell-Rafa <drafa0609@gmail.com>
2024-01-29 10:59:41 +0700

    finish value

--- app/components/WhyNeedUs.tsx
+++ app/components/WhyNeedUs.tsx
@@ -4,10 +4,13 @@ import React from "react";
 function WhyNeedUs() {
   return (
     <section id="why-need-us">
-      <h1 className="text-3xl font-medium p-16">Why You Need Us?</h1>
-      <div className="h-[500px] bg-[#FFF7F7] flex items-center p-16 sticky top-24">
-        <div className="flex justify-between items-center w-full">
-          <p className="text-2xl w-[715px] leading-relaxed font-medium">
+      <h1 className="text-3xl font-medium p-8 lg:p-16">Why You Need Us?</h1>
+      <div className="lg:h-[500px] bg-[#FFF7F7] lg:flex items-center px-8 py-16 lg:p-16 sticky top-24 relative">
+        <div className="flex justify-between items-center w-full flex-col lg:flex-row gap-16 lg:gap-0">
+          <p className="text-xl lg:text-2xl lg:w-[715px] leading-relaxed font-medium order-last lg:order-first">
+            <span className="text-lg block mb-2 absolute top-8 left-8 lg:static">
+              1/4
+            </span>
             Ibarat bayi burung elang yang baru menetas dan mulai mengenal dunia
             sekitarnya, Merek J&C Cookies ini juga baru akan memulai perjalanan
             besarnya...
@@ -18,12 +21,16 @@ function WhyNeedUs() {
             width={270}
             height={420}
             priority
+            className="w-[180px] h-[290px] lg:w-[270px] lg:h-[420px]"
           />
         </div>
       </div>
-      <div className="h-[500px] bg-[#BEF6FF] flex items-center p-16 sticky top-24">
-        <div className="flex justify-between items-center w-full">
-          <p className="text-2xl w-[715px] leading-relaxed font-medium">
+      <div className="lg:h-[500px] bg-[#BEF6FF] lg:flex items-center px-8 py-16 lg:p-16 sticky top-24 relative">
+        <div className="flex justify-between items-center w-full flex-col lg:flex-row gap-8 lg:gap-0">
+          <p className="text-xl lg:text-2xl lg:w-[715px] leading-relaxed font-medium order-last lg:order-first">
+            <span className="text-lg block mb-2 absolute top-8 left-8 lg:static">
+              2/4
+            </span>
             Seperti burung elang yang memiliki potensi pertumbuhan dan
             perkembangan yang besar, J&C Cookies ini juga berpotensi menjadi
             berkembang pesat dan dikenal di seluruh dunia.
@@ -37,9 +44,12 @@ function WhyNeedUs() {
           />
         </div>
       </div>
-      <div className="h-[500px] bg-[#FFFBDA] flex items-center p-16 sticky top-24">
-        <div className="flex justify-between items-center w-full">
-          <p className="text-2xl w-[715px] leading-relaxed font-medium">
+      <div className="lg:h-[500px] bg-[#FFFBDA] lg:flex items-center px-8 py-16 lg:p-16 sticky top-24 relative">
+        <div className="flex justify-between items-center w-full flex-col lg:flex-row gap-12 lg:gap-0">
+          <p className="text-xl lg:text-2xl lg:w-[715px] leading-relaxed font-medium order-last lg:order-first">
+            <span className="text-lg block mb-2 absolute top-8 left-8 lg:static">
+              3/4
+            </span>
             Namun kenyataannya, J&C Cookies dihadapkan pada beragam tantangan
             yang memicu kebingungan dalam strategi branding, kompetisi yang
             ketat, hingga kesulitan memikat klien
@@ -50,12 +60,16 @@ function WhyNeedUs() {
             width={500}
             height={250}
             priority
+            className="translate-x-8 translate-y-2"
           />
         </div>
       </div>
-      <div className="h-[500px] bg-[#E9F3FF] flex items-center p-16 sticky top-24">
-        <div className="flex justify-between items-center w-full">
-          <p className="text-2xl w-[715px] leading-relaxed font-medium">
+      <div className="lg:h-[500px] bg-[#E9F3FF] lg:flex items-center px-8 py-16 lg:p-16 sticky top-24 relative">
+        <div className="flex justify-between items-center w-full flex-col lg:flex-row gap-16 lg:gap-0">
+          <p className="text-xl lg:text-2xl lg:w-[715px] leading-relaxed font-medium order-last lg:order-first">
+            <span className="text-lg block mb-2 absolute top-8 left-8 lg:static">
+              4/4
+            </span>
             Untuk itulah Booster Indonesia hadir bersama Anda untuk mempercepat
             perkembangan merek Anda. Melalui layanan lengkap, kami menawarkan
             biaya yang fleksibel dengan hasil yang memuaskan.
@@ -66,6 +80,7 @@ function WhyNeedUs() {
             width={400}
             height={180}
             priority
+            className="translate-x-4 translate-y-2"
           />
         </div>
       </div>
